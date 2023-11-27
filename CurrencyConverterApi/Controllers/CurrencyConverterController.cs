f97bfa43bc6c52dc2a042f353d7909f5240785c2
Akshay Patil <53465888+AkshayPatil4@users.noreply.github.com>
2023-11-27 22:44:57 +0100

    Add project files.

---
+++ CurrencyConverterApi/Controllers/CurrencyConverterController.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CurrencyConverterApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyConverterController : ControllerBase
+    {
+        [HttpGet("convertToWords")]
+        public ActionResult<string> ConvertToWords(string amountStr)
+        {
+            // Replace commas with periods to ensure correct parsing
+            decimal amount;
+            if (!decimal.TryParse(amountStr.Replace(",", "."), out amount) || amount < 0 || amount > 999999999999999.99m)
+            {
+                return BadRequest("Invalid amount. Please provide a value between 0 and 999,999,999,999,999.99");
+            }
+
+            // Extract trillions, billions, millions, thousands, dollars, and cents from the input amount
+            long trillions = (long)Math.Floor(amount / 1000000000000);
+            long billions = (long)Math.Floor((amount - trillions * 1000000000000) / 1000000000);
+            long millions = (long)Math.Floor((amount - trillions * 1000000000000 - billions * 1000000000) / 1000000);
+            long thousands = (long)Math.Floor((amount - trillions * 1000000000000 - billions * 1000000000 - millions * 1000000) / 1000);
+            long dollars = (long)(amount - trillions * 1000000000000 - billions * 1000000000 - millions * 1000000 - thousands * 1000);
+            int cents = (int)((amount - trillions * 1000000000000 - billions * 1000000000 - millions * 1000000 - thousands * 1000 - dollars) * 100);
+
+            // Handle excess cents
+            if (cents > 99)
+            {
+                dollars += cents / 100;
+                cents %= 100;
+            }
+
+            string result = "";
+
+            // Convert trillions, billions, millions, thousands, dollars, and cents to words
+            if (trillions > 0)
+            {
+                result += $"{NumberToWords(trillions)} trillion ";
+            }
+
+            if (billions > 0)
+            {
+                result += $"{NumberToWords(billions)} billion ";
+            }
+
+            if (millions > 0)
+            {
+                result += $"{NumberToWords(millions)} million ";
+            }
+
+            if (thousands > 0)
+            {
+                result += $"{NumberToWords(thousands)} thousand ";
+            }
+
+            if (trillions == 0 && billions == 0 && millions == 0 && thousands == 0 && dollars == 0 && cents == 0)
+            {
+                return "zero dollars";
+            }
+
+
+            if (dollars > 0 || thousands > 0 || millions > 0 || billions > 0 || trillions > 0)
+            {
+                result += $"{NumberToWords(dollars)} dollars";
+            }
+
+            if (cents > 0)
+            {
+                if (dollars > 0)
+                {
+                    result += " and ";
+                }
+                else
+                {
+                    result += "zero dollars and ";
+                }
+
+                result += $"{NumberToWords(cents)} cents";
+            }
+
+            return result.Trim();
+        }
+
+        // Convert a number to words
+        private string NumberToWords(long number)
+        {
+            if (number == 0)
+            {
+                return "zero";
+            }
+
+            string[] units = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            string[] teens = { "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            string[] tens = { "", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+            string words = "";
+
+            // Convert hundreds
+            if ((number / 100) > 0)
+            {
+                words += units[number / 100] + " hundred ";
+                number %= 100;
+            }
+
+            // Convert tens and ones
+            if (number > 0)
+            {
+                if (words != "")
+                {
+                    words += "and ";
+                }
+
+                if (number < 10)
+                {
+                    words += units[number];
+                }
+                else if (number < 20 && number > 10)
+                {
+                    words += teens[number - 11];
+                }
+                else
+                {
+                    words += tens[number / 10];
+                    if ((number % 10) > 0)
+                    {
+                        words += "-" + units[number % 10];
+                    }
+                }
+            }
+
+            return words;
+        }
+    }
+}
---
+++ CurrencyConverterApi/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAngular", builder =>
+            {
+                builder.WithOrigins("http://localhost:4200")
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            });
+        });
+
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            // Add production error handling here.
+        }
+
+        app.UseRouting();
+
+        app.UseCors("AllowAngular");
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
